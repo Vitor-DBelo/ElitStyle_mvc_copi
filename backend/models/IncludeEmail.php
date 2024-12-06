8ae9cc677e6a2aff06b7b321860df60ad54c5cfc
Vitor-DBelo <vitordaniel2003g@gmail.com>
2024-12-05 23:12:00 -0300

    Update IncludeEmail.php

--- backend/models/IncludeEmail.php
+++ backend/models/IncludeEmail.php
@@ -8,7 +8,7 @@ use PHPMailer\PHPMailer\Exception;
 
 define("SMTP_HOST", "smtp.gmail.com");
 define("SMTP_PORT", 587);
-define("SMTP_USER", "elitestyle404@gmail.com"); 
+define("SMTP_USER", ""); 
 define("SMTP_PASS", "hbjlyujrzexojznv"); 
 
 ?>
